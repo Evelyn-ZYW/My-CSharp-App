40eb8647de5b6f46ed6f673f0e2022fc802426e4
Evie <bunnyroll@Evelyns-iMac.local>
2025-03-12 19:40:01 -0700

    iterating enum

--- Program.cs
+++ Program.cs
@@ -4,13 +4,16 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // strings and date are immutable by default, we cannot change them once they are instantiated
-        // we can reassign the variable, we'll get the expected behaviour
-        string name = "Evelyn";
-        name = name.ToLower();
-        Console.WriteLine(name);
-        Console.WriteLine(DateTime.UtcNow);
-        var date = DateTime.UtcNow.AddDays(1);
-        Console.WriteLine(date);
+        foreach (var e in Enum.GetValues(typeof(EmployeeType)))
+        {
+            Console.WriteLine(e);
+        }
     }
+}
+
+enum EmployeeType
+{
+    manager = 1,
+    supervisor = 2,
+    worker = 3
 }
--- obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac6df4114407b4bb6ba74e44d31a489fe9c39f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4e414cfb3ee7e6f6c0ba318ed63153f1e33c94")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
