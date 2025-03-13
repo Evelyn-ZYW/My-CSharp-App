950caf5a8aadce6e958e40ba0b994d0e63de6971
Evie <bunnyroll@Evelyns-iMac.local>
2025-03-12 17:09:17 -0700

    create AddNumbers method

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using System.Security.Cryptography;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        int a = 1;
+        int b = 2;
+        int c = AddNumbers(a, b);
+        Console.WriteLine(c);
         Console.WriteLine("Hello world!");
+    }
+    public static int AddNumbers(int a, int b)
+    {
+        return a + b;
     }
 }
